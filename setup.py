ddc68d7182951d9baced7032b7a603f579988899
Michael Lang <michi@chester.at>
2014-04-04 18:44:42 +0200

    * adjusted setup.py for pypi

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ except ImportError:
     from setuptools import setup, find_packages
 
 setup(name='device42api',
-      version='1.0',
+      version='1.2.1',
       description='Device42.com API module for object oriented API handling',
       author='Michael Lang',
       author_email='Michael.Lang@ctbto.org',
-      url='https://github.com/python-device42api/',
+      url='http://python-device42api.readthedocs.org/en/latest/',
       packages=find_packages(),
       license='GPLv2',
       classifiers=[
@@ -20,7 +20,7 @@ setup(name='device42api',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
-        'License :: OSI Approved :: GPL License',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
